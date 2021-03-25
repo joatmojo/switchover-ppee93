6612232fb1548432bf3408f9cdc64b41f1c216a0
Learn Git <trio.fajar18@gmail.com>
2021-03-25 16:40:57 +0700

    add shell

--- files/pgpool_attach.sh
+++ files/pgpool_attach.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export PATH=$PATH:/data/PostgresPlus/9.3AS/bin
-export PCPPASSFILE=./pcppass
+export PATH=$PATH:/usr/bin/
+export PCPPASSFILE=/data/pgpool/pcppass
 
 pcp_attach_node -U enterprisedb -h 192.168.131.138 -p 9898 -w -n 0
 pcp_attach_node -U enterprisedb -h 192.168.131.138 -p 9898 -w -n 1
