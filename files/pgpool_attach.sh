e2aa0000a76282cc7a3d8fcacb8f94bf28e88d22
Learn Git <trio.fajar18@gmail.com>
2021-04-28 22:23:06 +0700

    refixing

--- files/pgpool_attach.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:/usr/bin/
-export PCPPASSFILE=/data/pgpool/pcppass
-
-pcp_attach_node -U enterprisedb -h 192.168.131.138 -p 9898 -w -n 0
-pcp_attach_node -U enterprisedb -h 192.168.131.138 -p 9898 -w -n 1
---
+++ files/pgpool_attach_node1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PATH=$PATH:/usr/bin/
+export PCPPASSFILE=/data/pgpool/pcppass
+
+pcp_attach_node -U pcpuser -h 192.168.131.33 -p 9898 -w -n 0
+pcp_attach_node -U pcpuser -h 192.168.131.33 -p 9898 -w -n 1
