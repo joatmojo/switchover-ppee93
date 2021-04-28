d5eca3597a4633432d815da5ee2b47ace552771e
Learn Git <trio.fajar18@gmail.com>
2021-04-28 23:19:40 +0700

    return

--- files/pgpool_attach_node1.sh
+++ files/pgpool_attach_node1.sh
@@ -2,5 +2,5 @@
 export PATH=$PATH:/usr/bin/
 export PCPPASSFILE=/data/pgpool/pcppass
 
-pcp_attach_node -U pcpuser -h 192.168.131.34 -p 9898 -w -n 0
-pcp_attach_node -U pcpuser -h 192.168.131.34 -p 9898 -w -n 1
+pcp_attach_node -U pcpuser -h 192.168.131.33 -p 9898 -w -n 0
+pcp_attach_node -U pcpuser -h 192.168.131.33 -p 9898 -w -n 1
